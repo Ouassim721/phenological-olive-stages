a8ab32416936de1ee6e0eb051e34f64a01282c0b
Ouassim721 <derjaouassim2004@gmail.com>
2024-07-17 17:22:34 +0100

    message

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 //------------------------------------------------------------//
+app.get('/index', (req, res) => {
+    res.render('index');
+  });
+//------------------------------------------------------------//
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
+//------------------------------------------------------------//
+app.get('/recolte', (req, res) => {
+    res.render('recolte');
+  });
 //------------------------------------------------------------//
 app.set('view engine', 'ejs');
 
